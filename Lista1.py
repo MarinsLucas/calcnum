000e718468c617fc893e2e2005b239b92129874a
jooj211 <jdmctuber@gmail.com>
2022-09-28 19:13:53 -0300

    salvando

--- Lista1.py
+++ Lista1.py
@@ -3,14 +3,19 @@ import numpy as np
 import cmath as math
 
 
-def cooling(T0, k, T_s, t_end, dt, npoints): # Explicit Euler, Implicit Euler and Crank-Nicolson
+def cooling(T0, k, T_m, dt, npoints, type): # Explicit Euler, Implicit Euler and Crank-Nicolson
     k *= -1
     T = np.zeros(npoints)
     T[0] = T0
     for i in range(npoints-1):
-        ''' T[i+1] = dt*(k*(T[i] - T_s)) + T[i] ''' # Explicit Euler
-        ''' T[i+1] = (T[i] - k * dt * T_s)/(1-k*dt) ''' # Implicit Euler
-        T[i+1] = T[i] + dt*k*(T[i] + (1/2)*dt*k*(T[i] - T_s) - T_s) # Crank-Nicolson
+        if type == 1:
+            T[i+1] = dt*(k*(T[i] - T_m)) + T[i] # Explicit Euler
+        
+        if type == 2:
+            T[i+1] = (T[i] - k * dt * T_m)/(1-k*dt) # Implicit Euler
+        
+        if(type == 3): # Crank-Nicolson
+            T[i+1] = T[i] + dt*k*(T[i] + (1/2)*dt*k*(T[i] - T_m) - T_m) # Crank-Nicolson
         
     return T
 
@@ -18,23 +23,24 @@ def cooling(T0, k, T_s, t_end, dt, npoints): # Explicit Euler, Implicit Euler an
 def main():
     T0 = 99
     k = 0.0358
-    T_s = 27
+    T_m = 27
     t_ini = 0
     t_end = 100
     npoints = 10
 
-    dt = (t_end-t_ini)/(npoints-1)
-    ''' T, t = cooling(T0, k, T_s, t_end, dt, 0, npoints)
-    plt.plot(t, T, '-*')
-    T, t = cooling(T0, k, T_s, t_end, dt, 0.5, npoints)
-    plt.plot(t, T, '-o') '''
     t = np.linspace(t_ini,t_end,npoints)
-    T = cooling(T0, k, T_s, t_end, dt, npoints)
+    dt = (t_end-t_ini)/(npoints-1)
+    
+    T= cooling(T0, k, T_m, dt, npoints, 1)
+    plt.plot(t, T, '-*')
+    T = cooling(T0, k, T_m, dt, npoints, 2)
+    plt.plot(t, T, '-o')
+    T = cooling(T0, k, T_m, dt, npoints, 3)
     plt.plot(t, T, '-^')
 
     tt = np.linspace(t_ini, t_end, 200)
-    plt.plot(tt, ((T0 - T_s)*np.exp(-k*tt)) + T_s, '--')
-    plt.legend(['Crank-Nicolson', 'Exact'])
+    plt.plot(tt, ((T0 - T_m)*np.exp(-k*tt)) + T_m, '--')
+    plt.legend(['Explicit Euler', 'Implicit Euler', 'Crank-Nicolson', 'Exact'])
 
     plt.xlabel('tempo (s)')
     plt.ylabel('temperatura (C)')
