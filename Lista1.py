277a942f2b77b4b0c36ca2eeb31e600c325e7f0c
jooj211 <jdmctuber@gmail.com>
2022-09-28 17:49:02 -0300

    a

--- Lista1.py
+++ Lista1.py
@@ -3,42 +3,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cmath as math
 
-def resfriamentoNewtonExata():
-    thetaInicial = 99
-    thetaM = 27
-    dt = 0.01
-    k = 0.035871952
-    x = np.arange(0, 500, dt)
-    y = ((thetaInicial - thetaM)*np.exp(-k*x)) + thetaM        
-    return(x, y)
 
-def resfriamentoNewtonEulerExplicito(x,y):
-    refin = 10
-    erro = np.zeros(refin)
-    vetorDeltaT = np.zeros(refin)
-    condicaoInicial = 99
-    coeficienteDifusao = 0.1
-    tf = 500
-    ti = 0.0
+def resfriamentoNewtonExata(thetaInicial, thetaM, k, tt):
+    return ((thetaInicial - thetaM)*np.exp(-k*tt)) + thetaM
 
-    for k in range(0,refin):
-        quantPontos = 4**(k+1)+1
-        dt = (tf-ti)/(quantPontos - 1)
-        t = np.linspace(ti,tf,quantPontos)
-        f = np.zeros(quantPontos) 
-        f[0] = condicaoInicial
 
-        for i in range(0, quantPontos-1):
-            f[i+1] = (20.0/(20.0+coeficienteDifusao*dt))*f[i] 
-        plt.plot(t,f, '-*')
-        plt.plot(x,y)
-        plt.legend(['aproximada','exata'])
+refin = 10
+erro = np.zeros(refin)
+vetorDeltaT = np.zeros(refin)
+condicaoInicial = 99
+coeficienteDifusao = 0.358
+tf = 100.0
+ti = 0.0
+T_s = 27
+
+for k in range(0, refin):
+
+    quantPontos = 4**(k+1)+1
+    dt = (tf-ti)/(quantPontos - 1)
+
+    t = np.linspace(ti, tf, quantPontos)
+    f = np.zeros(quantPontos)
+    
+    f[0] = condicaoInicial
+
+    for n in range(0, quantPontos-1):
+        f[n+1] = (1.0-k*dt*(1.0-0.5))/(1.0+k*dt*-0.5) * \
+        f[n] + (k*dt*(1.0-0.5)*T_s)/(1.0+k*dt*0.5)
+
+        tt = np.linspace(ti, tf, 200)
+
+        plt.plot(t, f, '-*', tt, resfriamentoNewtonExata(condicaoInicial, T_s, coeficienteDifusao, tt))
+        plt.legend(['aproximada', 'exata'])
         plt.show()
-
-
-x,y = resfriamentoNewtonExata()
-resfriamentoNewtonEulerExplicito(x,y)
-
-plt.plot(x, y)
-plt.show()
-
--- codigoprofessor.py
+++ codigoprofessor.py
@@ -1,93 +1,45 @@
-'''
-Formulate a θ-rule for the three schemes (Forward Euler, Backward Euler and Crank-Nicolson) for Newton's law of cooling such that you can get the three schemes from a single formula by varying the θ parameter. Implement the θ scheme in a function cooling(T0, k, T_s, t_end, dt, theta=0.5), where T0 is the initial temperature, k is the heat transfer coefficient, T_s is the temperature of the surroundings, t_end is the end time of the simulation, dt is the time step, and theta corresponds to θ. The cooling function should return the temperature as an array T of values at the mesh points and the time mesh t. Construct verification examples to check that the implementation works.
-
-For verification, use the exact solution T(t) = (T0 - T_s) * e^(-kt) + T_s.
-Plot one graph for the approximations and the exact solution using pyplot.
-
-Hint: Use the following code as basis.
-'''
 import numpy as np
 import matplotlib.pyplot as plt
 
-def uexata(a,b,t):
-  return b*np.exp(-a*t)
+def uexata(a,b,t): #função exata
+  return b*np.exp(-a*t) # Solução exata
 
-ti = 0.0
-tf = 1.0
-refin = 5
-erro = np.zeros(refin)
-dtt = np.zeros(refin)
-for k in range(0,refin):
-  npoints = 4**(k+1)+1
-  dt = (tf-ti)/(npoints-1)
-  a = 1.0
-  # condicao inicial
-  b = 1.0
+ti = 0.0 # Tempo inicial
+tf = 1.0 # Tempo final
+refin = 5 # Refinamento
+
+erro = np.zeros(refin) # Vetor para armazenar os erros
+dtt = np.zeros(refin) # Vetor para armazenar os passos de tempo
+
+for k in range(0,refin): # Loop para refinamento
+    
+  npoints = 4**(k+1)+1 # Número de pontos
+  dt = (tf-ti)/(npoints-1) # Passo de tempo
+  
+  a = 1.0 # Coeficiente de difusão
+  b = 1.0 # Condicao inicial
+  
   # coordenadas
-  t = np.linspace(ti,tf,npoints)
-  u = np.zeros(npoints)
-  u[0] = b
+  t = np.linspace(ti,tf,npoints) # Vetor de tempo
+  u = np.zeros(npoints) # Vetor para armazenar a solução
+  u[0] = b # Condicao inicial
 
-  for n in range(0,npoints-1):
-    u[n+1] = (1.0/(1.0+a*dt))*u[n]
-    #u[n+1] = ((2.-a*dt)/(2.0+a*dt))*u[n]
+  for n in range(0,npoints-1): # Loop para resolução
+    u[n+1] = (1.0/(1.0+a*dt))*u[n] # Solução numérica
+    # u[n+1] = ((2.-a*dt)/(2.0+a*dt))*u[n]
 
-  tt = np.linspace(ti,tf,200)
-  plt.plot(t,u,'-*',tt,uexata(a,b,tt))
-  plt.legend(['aproximada','exata'])
-  plt.show()
-  erro[k] = np.max(np.abs(u-uexata(a,b,t)))
-  dtt[k] = dt
-  print(dt)
-plt.plot(-np.log(dtt),np.log(erro))
-plt.show()
-'''
+  tt = np.linspace(ti,tf,200) # Vetor de tempo para a solução exata
+  
+  plt.plot(t,u,'-*',tt,uexata(a,b,tt))  # Plotando a solução numérica e exata
+  plt.legend(['aproximada','exata']) # Legenda
+  plt.show() # Mostrando o gráfico
+  
+  erro[k] = np.max(np.abs(u-uexata(a,b,t))) # Calculando o erro
+  dtt[k] = dt # Armazenando o passo de tempo
+  
+  print(dt) # Mostrando o passo de tempo
 
-print((np.log(erro[-1])-np.log(erro[0]))/(np.log(dtt[-1])-np.log(dtt[0])))
+plt.plot(-np.log(dtt),np.log(erro)) # Plotando o erro em função do passo de tempo
+plt.show() # Mostrando o gráfico
 
-
-Filename: cooling.py.
-'''
-
-import numpy as np
-import matplotlib.pyplot as plt
-
-def uexata(T0, k, T_s, t):
-  return (T0 - T_s) * np.exp(-k * t) + T_s
-
-def cooling(T0, k, T_s, t_end, dt, theta=0.5):
-  npoints = int(t_end/dt)+1
-  t = np.linspace(0, t_end, npoints)
-  T = np.zeros(npoints)
-  T[0] = T0
-
-  for n in range(0, npoints-1):
-    T[n+1] = (1.0-k*dt*(1.0-theta))/(1.0+k*dt*theta)*T[n] + (k*dt*(1.0-theta)*T_s)/(1.0+k*dt*theta)
-
-  return T, t
-
-def main():
-  T0 = 1.0
-  k = 0.1
-  T_s = 0.0
-  t_end = 5.0
-  dt = 0.1
-
-  T, t = cooling(T0, k, T_s, t_end, dt, theta=0.5)
-  tt = np.linspace(0, t_end, 200)
-  plt.plot(t, T, '-*', tt, uexata(T0, k, T_s, tt))
-  plt.legend(['aproximada','exata'])
-  plt.show()
-  T, t = cooling(T0, k, T_s, t_end, dt, theta=0.0)
-  tt = np.linspace(0, t_end, 200)
-  plt.plot(t, T, '-*', tt, uexata(T0, k, T_s, tt))
-  plt.legend(['aproximada','exata'])
-  plt.show()
-  T, t = cooling(T0, k, T_s, t_end, dt, theta=1.0)
-  tt = np.linspace(0, t_end, 200)
-  plt.plot(t, T, '-*', tt, uexata(T0, k, T_s, tt))
-  plt.legend(['aproximada','exata'])
-  plt.show()
-
-if __name__ == '__main__':
-  main()
+print((np.log(erro[-1])-np.log(erro[0]))/(np.log(dtt[-1])-np.log(dtt[0]))) # Ordem de convergência
