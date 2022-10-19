1b23bc9a1143950ed966a1bfb75acbe4bb6490e3
Lucas Marins <lucasmarinsramalho@gmail.com>
2022-10-19 10:16:07 -0300

    improve:exata-solution

--- Lista2.py
+++ Lista2.py
@@ -7,17 +7,17 @@ from cmath import sqrt
 import matplotlib.pyplot as plt
 import numpy as np
 
-def solExata(eu):
+def solExata(epslon):
     t_ini = 0
     t_final = 1
-    c2 =  (pow(e, -(1/sqrt(eu))) - 1) / (pow(e, (1/sqrt(eu))) - pow(e, -1/sqrt(eu)))
-    c1 = 0 - 1 - c2
+    c2 =  (pow(e, (-1/sqrt(epslon))) - 1) / (pow(e, (1/sqrt(epslon))) - pow(e, (-1/sqrt(epslon))))
+    c1 = - 1 - c2
     x = np.linspace(t_ini, t_final, 200)
-    plt.plot( x, c1* pow(e, -(x/sqrt(eu)))  +  c2*pow(e, (x/sqrt(eu))+ 1) )
+    ux = c1* pow(e, (-x/sqrt(epslon)))  +  c2*pow(e, (x/sqrt(epslon))) + 1
+    plt.plot( x, ux, label = "Solução exata")
     plt.show()
 
 def main():
     solExata(0.1)
-
 
 main()
