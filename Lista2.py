8ce3e0fc00932d1fdc3699498370fae4048fb75a
Lucas Marins <lucasmarinsramalho@gmail.com>
2022-10-19 20:21:32 -0300

    deu certo, porém as cores do gráfico estão erradas

--- Lista2.py
+++ Lista2.py
@@ -5,61 +5,141 @@
 from math import e
 from cmath import sqrt
 import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
 import numpy as np
 
-""" def TDMASolve(a, b, c, d): 
-    #a,b,c são as diagonais 
-    #d é onde vamos armazenar a resposta
+def normaMaximo(x):
+    size = len(x)
+    maximo = np.abs(x[0])   
     
-    n = len(d)
-    nmax = n #??
+    for i in range(size):
+        temp = np.abs(x[i])        
+        if(temp > maximo):
+            maximo = temp
+            
+    return maximo
 
-    # Modifica o primeiro coeficiente de cada linha
-    c[0] /= b[0]
-    d[0] /= b[0]
+def distanciaMaximo(x1, x2):
+    if(len(x1) != len(x2)):
+        print("O tamanho dos vetores x1 e x2 precisa ser o mesmo")
+        return 0
+        
+    size = len(x1)
+    dist = abs(x1[0] - x2[0])  
     
-    # Modifica os coeficientes de cada linha
-    for i in range(1, nmax):
-        ptemp = b[i] - (a[i] * c[i-1]) 
-        c[i] /= ptemp
-        d[i] = (d[i] - a[i] * d[i-1])/ptemp
+    for i in range(size):
+        temp = abs(x1[i] - x2[i])
+        if(temp > dist):
+            dist = temp
+            
+    return dist
 
-    #Substituição de volta
-    x = [0 for i in range(nmax)]
-    x[-1] = d[-1]
-    for i in range(-2,- nmax-1,-1): 
-        x[i] = d[i] - c[i] * x[i+1] 
+def calculaErro(x_prox, x_atual):
+    return distanciaMaximo(x_prox, x_atual)/normaMaximo(x_prox)
 
-    return x """
 
-def solExata(epslon):
+def Tomas(a, b, c, d):
+    ''' Resolve Ax = d onde A e uma matriz tridiagonal composta pelos vetores a, b, c
+		a - subdiagonal
+        	b - diagonal principal
+		c - superdiagonal.
+	Retorna x
+    '''
+    #print('diagonais: ', a, b, c, d)
+    n = len(d) # len(d) == len(b)
+    c_ = [ c[0] / b[0] ]
+    d_ = [ d[0] / b[0] ]
+    
+    for i in range(1, n):
+        aux = b[i] - c_[i-1]*a[i-1]
+        if i < n-1:
+            c_.append( c[i] / aux )
+        d_.append( (d[i] - d_[i-1]*a[i-1])/aux )
+    
+    # Substituicao de volta
+    x = [d_[-1]]
+    for i in range(n-2, -1, -1):
+        x = [ d_[i] - c_[i]*x[0] ] + x
+    
+    return x
+
+def F(x, epslon):
     t_ini = 0
     t_final = 1
     c2 =  (pow(e, (-1/sqrt(epslon))) - 1) / (pow(e, (1/sqrt(epslon))) - pow(e, (-1/sqrt(epslon))))
     c1 = - 1 - c2
-    x = np.linspace(t_ini, t_final, 200)
     ux = c1* pow(e, (-x/sqrt(epslon)))  +  c2*pow(e, (x/sqrt(epslon))) + 1
-    plt.plot( x, ux, label = "Solução exata")
-    #plt.show()
+    return ux
 
 
-def difFinita(epslon, h, npoints):
-    U = np.zeros(npoints)
-    U[0] = 0
-    U[1] = 0
+def difFinita(epslon, h, npart):
     
-
-    for i in range(1 , npoints-1):
-        U[i+1] = ((U[i]-1)*(h*h))/epslon + 2*U[i] - U[i-1] 
-
-    x = np.float16(np.linspace(0, 1, npoints))
+    ordem = npart -1
+    diagonalSuperior = []
+    diagonalPrincipal = []
+    diagonalInferior = []
+    termoIndependente = []
+    solApprox = []
+    tempoExata = []
+    tempo = []
+    solExata = []
+    erro = []
     
-    plt.plot(x, U, '-*')
-    plt.show()
+    for i in range(ordem - 1):
+        diagonalSuperior.append(epslon * -1.0)
+        diagonalInferior.append(epslon * -1.0)
+    
+    for i in range(ordem):
+        diagonalPrincipal.append(2 * epslon + (h*h))
+        termoIndependente.append(h*h)
+    
+    particoes_exata = 100
+    
+    he = 1/float(particoes_exata-1)
+    for i in range(particoes_exata):
+        dhe = he * i
+        tempoExata.append(dhe)
+        solExata.append(F(dhe, epslon))
+
+    for i in range(npart + 1):
+        dh = h * i
+        tempo.append(dh)
+    
+    solApprox = Tomas(diagonalInferior, diagonalPrincipal, diagonalSuperior, termoIndependente)
+    
+    solApprox.insert(0, 0)
+    solApprox.append(0)
+
+    partErro = len(solApprox)
+    hErro = 1/float(partErro-1)
+    solExataErro = []
+    
+    for i in range (partErro):
+        dh = hErro * i
+        solExataErro.append(F(dh, epslon))
+        
+    erroNormaMax = calculaErro(solExataErro, solApprox)
+    print(erro)
+    print("Erro Norma Max: ", repr(erroNormaMax))
+    
+    
+    plt.plot(tempoExata, solExata, 'b--')
+    plt.plot(tempo, solApprox, '*--')
+    
+    plt.ylabel(u"Valor de u(h)")
+    plt.xlabel(u"Valor de h, " + str(npart) + u" partições")
+    
+    se_line = mlines.Line2D([], [], color='blue', marker='', markersize=0, label=u'Solução Exata')
+    ac_line = mlines.Line2D([], [], color='red', marker='', markersize=0, label=u'Solução Aprox.')
+    
+    plt.legend(handles=[se_line, ac_line])
+    
+    plt.title("Metodos de Resolucao")
+    plt.show() 
+
 
 def main():
-    solExata(0.1)   
-    difFinita(0.1, 1/50, 50)
-
+    # solExata(0.1)   
+    difFinita(0.1, 1/40, 41)
 
 main()
