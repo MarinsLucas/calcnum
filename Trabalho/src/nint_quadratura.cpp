7bc02f0559782b69cde6345939fdf5d817bc548e
Lucas Marins <lucasmarinsramalho@gmail.com>
2022-12-31 01:54:10 -0300

    acho que está funcionando

--- Trabalho/src/nint_quadratura.cpp
+++ Trabalho/src/nint_quadratura.cpp
@@ -1,56 +1,205 @@
 #include <iostream>
 #include <cmath>
+#include "sislin.h"
 
 using namespace std;
 
 #define MIN_SIMPSON_PART 1500
-//Definindo um nome para a variável de ponto flutuante para
-//facilitar possíveis trocas de precisão de ponto flutuante
-typedef long double fl;
+#define debug std::cout
+#define epslon 1E-8;
 
-//FIXME: essa função, está com precisão de 10e-20, porém era para estar exata. É erro de ponto flutuante!
-fl simpson_3_8(fl a, fl b, int pot)
+// Definindo um nome para a variável de ponto flutuante para
+// facilitar possíveis trocas de precisão de ponto flutuante
+typedef long double flu;
+
+// variáveis globais
+int N;
+
+// FIXME: essa função, está com precisão de 10e-20, porém era para estar exata. É erro de ponto flutuante!
+flu simpson_3_8(flu a, flu b, int pot)
 {
-    fl I = 0; 
-    //TODO: calcular simpson para no mínimo 1000 partições
-    fl h = fl((b-a)/MIN_SIMPSON_PART);
-    for(int i=0; i<MIN_SIMPSON_PART; i+=3)
+    flu I = 0;
+    // TODO: calcular simpson para no mínimo 1000 partições
+    flu h = flu((b - a) / MIN_SIMPSON_PART);
+    for (int i = 0; i < MIN_SIMPSON_PART; i += 3)
     {
-        I += (fl(3*h)/fl(8.0)) * (powl(a + i*h, pot) + 3*powl(a + (i+1)*h, pot) + 3*powl(a + (i+2)*h, pot) + powl(a +(i+3)*h, pot));
+        I += (flu(3 * h) / flu(8.0)) * (powl(a + i * h, pot) + 3 * powl(a + (i + 1) * h, pot) + 3 * powl(a + (i + 2) * h, pot) + powl(a + (i + 3) * h, pot));
     }
     return I;
 }
+
+// retorna um valor para f_j
+flu* f(flu *w, flu *t, flu *g)
+{
+    flu *f = new flu[2*N];
+    for(int j = 0; j<2*N; j++)
+    {
+        f[j] = 0; 
+        for (int i = 0; i < N; i++)
+        {
+            f[j] += w[i] * pow(t[i], j);
+        }
+        f[j] -= g[j];
+    }
+    return f;
+}
+
+flu* f4(flu *w, flu *t, flu *g)
+{
+    flu *f = new flu[2*N];
+    for(int j = 0; j<2*N; j++)
+    {
+        f[j] = 0; 
+        for (int i = 0; i < N; i++)
+        {
+            f[j] += w[i] * pow(t[i], j);
+        }
+        f[j] -= g[j];
+        f[j] *= -1; 
+    }
+    return f;
+}
+
+flu* f3(flu *w, flu *t, flu *g, int k)
+{
+    flu *f = new flu[2*N];
+    for(int j = 0; j<2*N; j++)
+    {
+        f[j] = 0; 
+        for (int i = 0; i < N; i++)
+        {
+            if(i == k)
+            {
+                flu aux = t[i] + epslon;
+                f[j] = w[i] + pow(aux, j);
+            }
+            else
+            {
+                f[j] += w[i] * pow(t[i], j);
+            }
+        }
+        f[j] -= g[j];
+    }
+    return f;
+}
+
+flu* f2(flu *w, flu *t, flu *g, int k)
+{
+    flu *f = new flu[2*N];
+    for(int j = 0; j<2*N; j++)
+    {
+        f[j] = 0; 
+        for (int i = 0; i < N; i++)
+        {
+            if(i == k)
+            {
+                f[j] += w[i];
+                f[j] += epslon;
+                f[j] *= pow(t[i], j);
+            }
+            else
+            {
+                f[j] += w[i] * pow(t[i], j);
+            }
+        }
+        f[j] -= g[j];
+    }
+    return f;
+}
+
+// TODO: função que cria matriz jacobiana
+flu **Jacobiana(flu* w, flu* t, flu* g)
+{
+    flu** jaco = new flu*[2*N];
+    for (int j = 0; j < 2 * N; j++)
+    {
+        jaco[j] = new flu[2*N];
+        //percorre as colunas
+        for(int i = 0; i < N; i++)
+        {
+            //soma-se epslon na iesima posição de w
+            jaco[j][i] = ((f2(w, t, g, i)[j] - f(w, t, g)[j]))/epslon;
+            //debug<<jaco[j][i]<<endl;
+
+        }
+        for(int i = N; i<2*N ;i++)
+        {
+            //soma-se epslon na iesima posição de t
+            jaco[j][i] = ((f3(w, t, g, i-N)[j] - f(w, t, g)[j]))/epslon; 
+            //debug<<jaco[j][i]<<endl;
+
+        }
+    }
+    return jaco;
+}
+
+// TODO: resolução de sistema linear pelo método de newton
+void newtonSisNLin(flu *w, flu *t, flu *g) // J é a matriz jacobiana, e f é o sistema de funções não lineares
+{
+    int kmax = 100; 
+    flu** j; 
+    flu* fntion;
+    vector<flu> solution;
+    // Processo iterativo:
+    for (int k = 0; k < kmax; k++)
+    {
+        fntion = f4(w, t, g);
+        j = Jacobiana(w, t, g);
+        solution = gauss_pivoteamento(j, fntion, 2*N);
+
+        /* for(int i=0;i<2*N; i++)
+        {
+            for(int k=0 ; k<2*N; k++)
+            {
+                debug<<j[i][k]<<";";
+            }
+            debug<<endl;
+        } */
+        
+        /* for(int i = 0; i<2*N; i++)
+        {
+            debug<<solution[i]<<endl;
+        }    
+        debug<<endl;  */
+
+        for(int i = 0; i<N; i++)
+        {
+            w[i] = solution[i]*w[i] + w[i];
+            t[i] = solution[i]*t[i] + t[i]; 
+        } 
+    }
+}
+
 int main()
 {
-    //TODO: Definir o domínio de integração
-    fl a, b;
-    cout<<"Definir domínio de integração [a,b]: ";
-    cin>>a>>b;
+    // TODO: Definir o domínio de integração
+    flu a, b;
+    cout << "Definir domínio de integração [a,b]: ";
+    cin >> a >> b;
 
-    //TODO: Definir o número de integração N (lembrando que N pontos = 2N incógnitas)
-    int N;
-    cout<<"Definir o número de pontos de integração N: ";
-    cin>>N;
+    // TODO: Definir o número de integração N (lembrando que N pontos = 2N incógnitas)
+    cout << "Definir o número de pontos de integração N: ";
+    cin >> N;
 
-    //TODO: Definir condição inicial w_0 e t_0, com N entradas cada uma, para o método iterativo de Newton (relações descritas no roteiro)
-    fl *w0 = new fl[N];
-    fl *t0 = new fl[N];
+    // TODO: Definir condição inicial w_0 e t_0, com N entradas cada uma, para o método iterativo de Newton (relações descritas no roteiro)
+    flu *w0 = new flu[N];
+    flu *t0 = new flu[N];
 
-    for(int i =0; i <N/2; i++)
+    for (int i = 0; i < N / 2; i++)
     {
-        w0[i] = fl((b-a)*(i+1))/fl(2*N);
-        t0[i] = fl(a + (i+1)*(w0[i]/2));
-        w0[N-1-i] = w0[i];
-        t0[N-1-i] = (a+b) - t0[i];
-    }
-    
-    if(N%2 == 1)
-    {
-        t0[N/2] = (a+b)/fl(2); 
-        w0[N/2] =  fl((b-a)*(N/fl(2)))/fl(2*N); //FIXME: esse valor está correto?
+        w0[i] = flu((b - a) * (i + 1)) / flu(2 * N);
+        t0[i] = flu(a + (i + 1) * (w0[i] / 2));
+        w0[N - 1 - i] = w0[i];
+        t0[N - 1 - i] = (a + b) - t0[i];
     }
 
-    //!para debuging
+    if (N % 2 == 1)
+    {
+        t0[N / 2] = (a + b) / flu(2);
+        w0[N / 2] = flu((b - a) * (N / flu(2))) / flu(2 * N); // FIXME: esse valor está correto?
+    }
+
+    //! para debuging
     /* for(int i =0; i < N;i++)
     {
         cout<<w0[i]<<" ";
@@ -58,15 +207,22 @@ int main()
 
     cout.precision(12);
 
-    //TODO: criar vetor com integrais de x^i para criar um sistema linear bitelo
-    fl *g = new fl[2*N]; //g = vetor de integrais de x^i
-    for(int i=0; i<2*N; i++)
+    // TODO: criar vetor com integrais de x^i para criar um sistema linear bitelo
+    flu *g = new flu[2 * N]; // g = vetor de integrais de x^i
+    for (int i = 0; i < 2 * N; i++)
     {
         g[i] = simpson_3_8(a, b, i);
-        cout<<g[i]<<" ";
     }
 
-    //TODO: Montar matriz Jacobiana formada pela derivada da função f(w,t) (descrito no roteiro)
-    
-    //TODO: Escolher uma tolerancia na ordem de 10e-8 ou menos!
+    // TODO: Montar sistema linear de funções f(w,t)
+    /* for (int j = 0; j < 2 * N; j++)
+    {
+        debug << f(w0, t0, g)[j] << endl;
+    } */
+
+
+    // TODO: Montar matriz Jacobiana formada pela derivada da função f(w,t) (descrito no roteiro)
+    newtonSisNLin(w0, t0, g);
+
+    // TODO: Escolher uma tolerancia na ordem de 10e-8 ou menos!
 }
---
+++ Trabalho/src/sislin.h
@@ -0,0 +1,192 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef long double flu;
+
+typedef vector<vector<flu>> Matrix;
+typedef vector<flu> Vector;
+
+//FIXME: Acho que dá pra refazer essa função com ponteiros!!
+void troca_linha(flu** A, int i, int j, int n)
+{
+    double temp_i, temp_j;
+    for (int k = 0; k < n + 1; k++)
+    {
+        temp_i = A[i][k];
+        temp_j = A[j][k];
+        A[i][k] = temp_j;
+        A[j][k] = temp_i;
+    }
+}
+
+flu** matriz_aumentada(flu** A, flu* B, int n)
+{
+    // Cria uma nova matriz com n linhas e n + 1 colunas e adiciona a coluna B à matriz A
+    flu** M = new flu*[n];
+
+    for (int i = 0; i < n; i++)
+    { 
+        M[i] = new flu[n+1];
+        for (int j = 0; j < n; j++)
+        {
+            M[i][j] = A[i][j];
+        }
+        M[i][n] = B[i];
+    }
+
+    return M;
+}
+
+Vector gauss_pivoteamento(flu** A, flu* B, int n)
+{
+    vector<flu> solucao(n, 0.0);
+
+    A = matriz_aumentada(A, B, n);
+
+    // Eliminação progressiva
+    for (int k = 0; k < n; k++)
+    {
+        // Inicializa o maior elemento para índice e pivô
+        int i_max = k;
+        double pivo_max = A[i_max][k];
+
+        // Encontra o maior elemento da coluna e associa ao pivô
+        for (int i = k + 1; i < n; i++)
+        {
+            if (abs(A[i][k]) > pivo_max)
+            {
+                pivo_max = A[i][k];
+                i_max = i;
+            }
+        }
+
+        if (A[k][i_max] == 0)
+        { // Se o pivô for zero
+            std::cout << "Divisao por zero detectada!" << endl;
+            exit(1);
+        }
+
+        // Troca a linha do maior elemento com a linha atual
+        if (i_max != k)
+        {
+            troca_linha(A, k, i_max, n);
+        }
+
+        for (int i = k + 1; i < n; i++)
+        {
+            // fatora f para zerar os elementos abaixo do pivô
+            double f = (double)A[i][k] / (double)A[k][k]; // TODO: verificar se não é necessário converter para float128
+
+            // subtrai a linha do pivô multiplicada pelo fator f
+            for (int j = k + 1; j < n + 1; j++)
+            {
+                A[i][j] = A[i][j] - f * A[k][j];
+            }
+
+            // preenche a matriz triangular inferior com zeros
+            A[i][k] = 0;
+        }
+    }
+
+    // Substituição regressiva
+    for (int i = n - 1; i >= 0; i--)
+    {
+        // Inicializa o vetor solução com a última coluna da matriz
+        solucao[i] = A[i][n];
+
+        // Subtrai os elementos da solução já encontrados
+        // j é i + 1 pois os elementos da diagonal principal já foram zerados
+        for (int j = i + 1; j < n; j++)
+        {
+            solucao[i] = solucao[i] - A[i][j] * solucao[j];
+        }
+
+        // Divide o elemento da solução pelo elemento da diagonal principal
+        solucao[i] = (double)solucao[i] / (double)A[i][i];
+    }
+
+    return solucao;
+}
+
+vector<flu> decomposicao_LU(flu** M, flu* B, int ordem)
+{
+    if (ordem == 0)
+    {
+        return {};
+    }
+
+    vector<flu> y(ordem);
+    vector<flu> x(ordem);
+    flu** L = new flu*[ordem];
+    flu** U = new flu*[ordem];
+    for(int i=0; i<ordem; i++)
+    {
+        L[i] = new flu[ordem];
+        U[i] = new flu[ordem];
+    }
+
+    // percorre as linhas da A
+    for (int j = 0; j < ordem; j++)
+    {
+        U[0][j] = M[0][j];
+    }
+
+    for (int i = 0; i < ordem; i++)
+    {
+        L[i][0] = (double)M[i][0] / (double)U[0][0];
+    }
+
+    for (int i = 0; i < ordem; i++)
+    {
+        // Calcula L
+        for (int j = 0; j < i+1; j++)
+        {
+            flu soma = 0.0;
+            for (int k = 0; k < j; k++)
+            {
+                soma += L[i][k] * U[k][j];
+            }
+            L[i][j] = M[i][j] - soma;
+        }
+
+        // Calcula U
+        for (int j = i; j < ordem; j++)
+        {
+            flu soma = 0.0;
+            for (int k = 0; k < i; k++)
+            {
+                soma += L[i][k] * U[k][j];
+            }
+            U[i][j] = (M[i][j] - soma) / L[i][i];
+        }
+        //imprimeMatriz(L);
+    }
+
+    // Resolução L * y = b
+
+    y[0] = B[0] / L[0][0];
+    for (int i = 1; i < ordem; i++)
+    {
+        flu soma = 0.0;
+        for (int j = 0; j < i; j++)
+        {
+            soma += L[i][j] * y[j];
+        }
+        y[i] = (B[i] - soma) / L[i][i];
+    }
+
+    // Resolucao U * x = y
+    x[ordem - 1] = y[ordem - 1] / U[ordem - 1][ordem - 1];
+    for (int i = ordem - 1; i > -1; i--)
+    {
+        flu soma = y[i];
+        for (int j = i + 1; j < ordem; j++)
+        {
+            soma = soma - U[i][j] * x[j];
+        }
+        x[i] = soma / U[i][i];
+    }
+    return x;
+}
